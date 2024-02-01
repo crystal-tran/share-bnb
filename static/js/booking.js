5bd6f64efe378c816e96bb84de1ca16fac21967b
Brandie Lucano <blucano.dev@gmail.com>
2024-02-01 14:09:28 -0800

    added book/cancel forms in html and added functions for handling book/cancel in js

---
+++ static/js/booking.js
@@ -0,0 +1,55 @@
+"use strict";
+
+$(async function () {
+  $("#book").on("click", book);
+  $("#cancel").on("click", cancel);
+
+  let response = await fetch("/api/bookings", { params: { listing_id: listingId } });
+  let result = response.data;
+  console.log(response.data);
+
+  if ("error" in result) {
+    console.log(result.error);
+  } else {
+    let bookings = result.bookings;
+    if (bookings) $("#cancel").show();
+    else $("#book").show();
+  }
+});
+
+async function book(evt) {
+  evt.preventDefault();
+
+  let response = await fetch("/api/book",
+    {
+      method:"POST",
+      body:JSON.stringify({ listing_id: listingId })
+    });
+
+  let result = response.data;
+
+  if ("error" in result) {
+    console.log(result.error);
+  } else {
+    $("#book").hide();
+    $("#cancel").show();
+  }
+}
+
+async function cancel(evt) {
+  evt.preventDefault();
+
+  let response = await fetch("/api/cancel",
+  {
+    method:"POST",
+    body:JSON.stringify({ listing_id: listingId })
+  });
+  let result = response.data;
+
+  if ("error" in result) {
+    console.log(result.error);
+  } else {
+    $("#cancel").hide();
+    $("#book").show();
+  }
+}
